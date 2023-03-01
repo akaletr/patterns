f02d94fa8b29e919d0032b051efcbe914e08ec60
akaletr <dmitriy.polyakov@onlinesup.com>
2023-03-01 14:15:20 -0300

    retry pattern

--- internal/functions/simple.go
+++ internal/functions/simple.go
@@ -1,6 +1,7 @@
 package functions
 
 import (
+	"context"
 	"errors"
 	"math/rand"
 	"time"
@@ -10,6 +11,21 @@ type Function func(...interface{}) (interface{}, error)
 
 func NewSimpleFunc(timeout time.Duration, errorPercent int) Function {
 	return func(...interface{}) (interface{}, error) {
+		time.Sleep(timeout)
+		result := rand.Intn(100)
+
+		if result < errorPercent {
+			return 0, errors.New("error: something went wrong")
+		}
+
+		return result, nil
+	}
+}
+
+type FunctionWithContext func(context.Context, ...interface{}) (interface{}, error)
+
+func NewSimpleFuncWithContext(timeout time.Duration, errorPercent int) FunctionWithContext {
+	return func(ctx context.Context, i ...interface{}) (interface{}, error) {
 		time.Sleep(timeout)
 		result := rand.Intn(100)
 
--- internal/patterns/retry/retry.go
+++ internal/patterns/retry/retry.go
@@ -1,1 +1,24 @@
 package retry
+
+import (
+	"context"
+	"time"
+
+	"cmd/main/main.go/internal/functions"
+)
+
+func Retry(ctx context.Context, callback functions.FunctionWithContext, retryCount int, delay time.Duration) functions.FunctionWithContext {
+	return func(ctx context.Context, options ...interface{}) (interface{}, error) {
+		for r := 0; ; r++ {
+			result, err := callback(ctx)
+			if err == nil || r >= retryCount {
+				return result, err
+			}
+			select {
+			case <-time.After(delay):
+			case <-ctx.Done():
+				return nil, ctx.Err()
+			}
+		}
+	}
+}
